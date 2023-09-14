44de7f23fa044430ea466375b14463d410f68b51
Gustavo Navarro <genr1818@gmail.com>
2023-09-13 18:26:39 -0600

    StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -20,9 +21,9 @@ const composedEnhancers = compose(
 const store = createStore(pokemonsReducer, composedEnhancers);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 )
